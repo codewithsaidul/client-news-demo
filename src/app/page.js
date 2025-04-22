9d9a501c208f5931f4d3e2ea8136e93a7b567eea
codewithsaidul <codewithsaidul@gmail.com>
2025-04-22 12:50:02 +0600

    shadcn install & color setup complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-      <div className="text-8xl text-rose-400">Hello, This is News Website</div>
+      <div className="text-8xl text-rose-400 font-merriweather">Hello, This is News Website</div>
   );
 }
