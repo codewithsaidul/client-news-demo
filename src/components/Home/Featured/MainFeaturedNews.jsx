8cf51e7043700203a86ba7d18ce682df8c3aad79
codewithsaidul <codewithsaidul@gmail.com>
2025-06-02 16:26:50 +0600

    trying to solved text overlaping issue

--- src/components/Home/Featured/MainFeaturedNews.jsx
+++ src/components/Home/Featured/MainFeaturedNews.jsx
@@ -2,9 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import FeatureBottom from "./FeatureBottom";
 
-const MainFeaturedNews = ( { featuredNews } ) => {
-
-
+const MainFeaturedNews = ({ featuredNews }) => {
   const { _id, title, description, category, thumbnail } = featuredNews[0];
 
   return (
@@ -22,25 +20,25 @@ const MainFeaturedNews = ( { featuredNews } ) => {
           </figure>
 
           <div
-            className="absolute bottom-0 w-full h-[80%] bg__gradient"
-            style={{
-              zIndex: 1, // To ensure overlay is above the background image
-            }}
+            className="absolute bottom-0 w-full h-[80%] bg__gradient z-[1]"
           />
 
           <div className="text-white absolute bottom-4 left-2 min-[540px]:bottom-12 min-[540px]:left-5 z-2 w-[90%] h-auto">
             {/* max-[430px]:text-xl */}
             <span className="text-base capitalize">{category}</span>
-            <h2 className="text-xl sm:text-4xl md:text-4xl font-bold font-title my-2">
+            <h2 className="max-[350px]:text-base sm:text-3xl md:text-4xl font-bold font-title my-2 leading-snug break-words">
               {title}
             </h2>
+
             <p className="text-base font-medium line-clamp-2">{description}</p>
           </div>
         </Link>
       </div>
 
       <div className="mt-5 flex flex-col lg:flex-row max-lg:gap-8 gap-5">
-        {featuredNews.slice(1, 3).map((news) => <FeatureBottom key={news._id} news={news} />)}
+        {featuredNews.slice(1, 3).map((news) => (
+          <FeatureBottom key={news._id} news={news} />
+        ))}
       </div>
     </div>
   );
